ba98f4645e73ea6f00b6ddb5ef7e52c60c8b3bf2
supermenxxx <344593137@qq.com>
2018-03-05 08:56:32 +0800

    修改：1.用户接口增加自定义分页

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path, re_path,include
 from rest_framework.routers import DefaultRouter
-from django.urls import path, re_path
-from apps.account import views
-from apps.account import views as loon_user_views
 
-# router = DefaultRouter()
-# router.register('', views.LoonUserListViewSet)
-#
-# urlpatterns = [
-#     path(r'', include(router.urls))
-# ]
+from apps.account import views
+
+
+router = DefaultRouter()
+router.register('', views.LoonUserViewSet)
+
 urlpatterns = [
-    path('users/', loon_user_views.LoonUserList.as_view()),
-    re_path(r'^users/(\d+)/$', loon_user_views.LoonUserDetail.as_view()),
+    path(r'', include(router.urls))
 ]
--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,75 +1,44 @@
-from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework import viewsets
+from rest_framework import pagination
+from rest_framework.response import Response
 from apps.account.models import LoonUser
 from apps.account.serializers import LoonUserSerializer
-from service import format_response
 
 
-# class LoonUserListPagination(PageNumberPagination):
-#     """
-#     自定义用户列表分页
-#     """
-#     max_page_size = 100
-#     page_size = 10
-
-
-# class LoonUserListViewSet(viewsets.ReadOnlyModelViewSet):
-#     """
-#     用户列表及用户详情
-#     """
-#     queryset = LoonUser.objects.filter(is_deleted=False)
-#     serializer_class = LoonUserSerializer
-#     permission_classes = [AllowAny]
-#     pagination_class = LoonUserListPagination
-#
-#     def finalize_response(self, request, response, *args, **kwargs):
-#         res = super(LoonUserListViewSet, self).finalize_response(request, response, *args, **kwargs)
-#         if res.status_code < 400:
-#             res.data = {"code": 1, "msg": "success", "data": response.data}
-#         elif res.status_code > 400:
-#             res.data = {"code": res.status_code, "msg": response.data['detail'], "data": {}}
-#         elif res.status_code == 400:
-#             res.data = {"code": res.status_code, "msg": response.data, "data": {}}
-#         return res
-
-
-class LoonUserDetail(APIView):
+class LoonUserListPagination(pagination.PageNumberPagination):
     """
-    用户详情
+    自定义用户列表分页
     """
+    max_page_size = 50
+    page_size = 10
+    page_size_query_param = 'page_size'
+    def get_paginated_response(self, data):
+        return Response({
+            'total_pages':self.page.paginator.num_pages,
+            'page_size':self.page_size,
+            'count':self.page.paginator.count,
+            'current_page':self.page.number,
+            'value': data
+
+        })
+
+class LoonUserViewSet(viewsets.ModelViewSet):
+    """
+    用户列表及用户详情
+    """
+    queryset = LoonUser.objects.filter(is_deleted=False)
+    serializer_class = LoonUserSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = LoonUserListPagination
 
-    def get(self, request, pk):
-        print(request.user.username)
-        user = LoonUser.objects.filter(is_deleted=False, id=pk)
-        if user:
-            data = LoonUserSerializer(user.first())
-            msg = ''
-        else:
-            data = {}
-            msg = '不存在或已删除'
-        return format_response.JsonResponse(data=data.data, code=status.HTTP_200_OK, msg=msg)
+    def finalize_response(self, request, response, *args, **kwargs):
+        res = super(LoonUserViewSet, self).finalize_response(request, response, *args, **kwargs)
+        if res.status_code < 400:
+            res.data = {"code": 200, "msg": "success", "data": response.data}
+        elif res.status_code > 400:
+            res.data = {"code": res.status_code, "msg": response.data['detail'], "data": {}}
+        elif res.status_code == 400:
+            res.data = {"code": res.status_code, "msg": response.data, "data": {}}
+        return res
 
-
-class LoonUserList(APIView):
-    """
-    用户列表
-    """
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        print(request.COOKIES)
-        args = request.GET
-        per_page = int(args.get('per_page', 10)) if args.get('per_page', 10) else 10
-        page = int(args.get('page', 1)) if args.get('page', 1) else 1
-
-        total = LoonUser.objects.filter(is_deleted=False).count()
-
-        user_serializer_list = [LoonUserSerializer(user) for user in LoonUser.objects.filter(is_deleted=False)]
-        user_serializer_list = [user_serializer.data for user_serializer in user_serializer_list]
-
-        return format_response.JsonResponse(data=user_serializer_list, code=status.HTTP_200_OK,
-                                            msg='', per_page=per_page, page=page, total=total)
