97755e2d77a0f85bd306fdc9d3c2810ba9e97411
blackholll <blackholll@163.com>
2018-05-11 06:41:28 +0800

    add some help_text in models

--- apps/workflow/models.py
+++ apps/workflow/models.py
@@ -37,13 +37,13 @@ class State(models.Model):
     workflow_id = models.IntegerField('工作流')
     sub_workflow_id = models.IntegerField('子工作流id(如果需要在此状态启用子工单的话)', default=0, blank=True)
     is_hidden = models.BooleanField('是否隐藏', default=0, help_text='设置为True时,工单步骤中不显示此状态(当前处于此状态时除外)')
-    order_id = models.IntegerField('状态顺序', default=0, help_text='用于工单步骤接口时，step上状态的顺序(因为存在网状情况，所以需要认为设定顺序),值越小越靠前')
-    type_id = models.IntegerField('状态类型id', default=0, help_text='见service.constant_service中定义')
+    order_id = models.IntegerField('状态顺序', default=0, help_text='用于工单步骤接口时，step上状态的顺序(因为存在网状情况，所以需要人为设定顺序),值越小越靠前')
+    type_id = models.IntegerField('状态类型id', default=0, help_text='0.普通类型 1.初始状态(用于新建工单时,获取对应的字段必填及transition信息) 2.结束状态(此状态下的工单不得再处理，即没有对应的transition)')
 
-    participant_type_id = models.IntegerField('参与者类型id', default=1, blank=True, help_text='见service.constant_service中定义')
-    participant = models.CharField('参与者', default='', blank=True, max_length=100, help_text='可以为username\多个username(以,隔开)\部门id\角色id\变量id等')
+    participant_type_id = models.IntegerField('参与者类型id', default=1, blank=True, help_text='1.个人,2.多人,3.部门,4.角色,5.变量(支持工单创建人,创建人的leader),6.脚本,7.工单的字段内容(如表单中的"测试负责人"，需要为用户名或者逗号隔开的多个用户名),8.父工单的字段内容')
+    participant = models.CharField('参与者', default='', blank=True, max_length=100, help_text='可以为username\多个username(以,隔开)\部门id\角色id\变量(creator,creator_tl)等')
 
-    distribute_type_id = models.IntegerField('分配方式', default=1, help_text='见service.constant_service中定义')  # 1主动接单, 2直接处理,3.随机分配,4.全部处理(要求所有参与人都要处理一遍)
+    distribute_type_id = models.IntegerField('分配方式', default=1, help_text='1.主动接单(如果当前处理人实际为多人的时候，需要先接单才能处理) 2.直接处理(即使当前处理人实际为多人，也可以直接处理) 3.随机分配(如果实际为多人，则系统会随机分配给其中一个人。v0.2版本支持) 4.全部处理(要求所有参与人都要处理一遍,才能进入下一步。v0.2版本支持)')
     state_field_str = models.TextField('表单字段', default='{}', help_text='json格式字典存储,包括读写属性1：只读，2：必填，3：可选. 示例：{"created_at":1,"title":2, "sn":1}')  # json格式存储,包括读写属性1：只读，2：必填，3：可选，4：不显示, 字典的字典
     label = models.CharField('状态标签', max_length=1000, default='{}', help_text='json格式，由调用方根据实际定制需求自行确定,如状态下需要显示哪些前端组件:{"components":[{"AppList":1, "ProjectList":7}]}')
 
@@ -83,8 +83,8 @@ class Transition(models.Model):
 class CustomField(models.Model):
     """自定义字段, 设定某个工作流有哪些自定义字段"""
     workflow_id = models.IntegerField('工作流id')
-    field_type_id = models.IntegerField('类型', help_text='见service.constant_service中定义')
-    field_key = models.CharField('字段标识', max_length=50)
+    field_type_id = models.IntegerField('类型', help_text='5.字符串，10.整形，15.浮点型，20.布尔，25.日期，30.日期时间，35.单选框，40.多选框，45.下拉列表，50.多选下拉列表，55.文本域，60.用户名')
+    field_key = models.CharField('字段标识', max_length=50, help_text='字段类型请尽量特殊，避免与系统中关键字冲突')
     field_name = models.CharField('字段名称', max_length=50)
     order_id = models.IntegerField('排序', default=0)
     default_value = models.CharField('默认值', null=True, blank=True, max_length=100)
