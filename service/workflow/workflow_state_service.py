5bd7974b6620750689eeb2ed98fbe92d31289262
blackholll <blackholll@163.com>
2018-11-07 23:40:02 +0800

    field_list in init_state api should contain label

--- service/workflow/workflow_state_service.py
+++ service/workflow/workflow_state_service.py
@@ -158,6 +158,7 @@ class WorkflowStateService(BaseService):
                                    field_template=custom_field_dict[key]['field_template'],
                                    boolean_field_display=json.loads(custom_field_dict[key]['boolean_field_display']) if custom_field_dict[key]['boolean_field_display'] else {},  # 之前model允许为空了，为了兼容先这么写,
                                    field_choice=json.loads(custom_field_dict[key]['field_choice']),
+                                   label=json.loads(custom_field_dict[key]['label'])
                                    ))
 
         state_field_dict = json.loads(init_state_obj.state_field_str)
