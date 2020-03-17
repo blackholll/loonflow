c504499c82715184243fc3fcfe416a7d502741b8
children1987 <39188043@qq.com>
2020-03-17 17:23:35 +0800

    代码等效优化，减少重复代码

--- static/dist/js/workflow/workflow_manage.js
+++ static/dist/js/workflow/workflow_manage.js
@@ -1,27 +1,4 @@
-function initSelect2Items(url, dom, makeOption, selectedId) {
-  let param = {};
-  param.per_page = 1000;
-  param.page = 1
-  $.ajax({
-    type: "GET",
-    url: url,
-    cache: false,  //禁用缓存
-    data: param,  //传入组装的参数
-    dataType: "json",
-    success: function (result) {
-      $(dom).empty();
-      result.data.value.map(function (currentValue, index, arr) {
-        console.log('currentValue')
-        console.log(currentValue)
-        $(dom).append(
-          makeOption(currentValue)
-        );
-      })
-      $(dom).val(selectedId).trigger("change");
-    },
-  });
-}
-
+document.write("<script language=javascript src='/static/dist/js/common/useSelect2.js'></script>");
 $("#workflow_form").validate();
 
 $('#customNoticeSelect').select2({placeholderOption: "first", allowClear:true});
