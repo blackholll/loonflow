b0024859030d6cfeaf4fd95cf604e24e60f1e6c8
blackholll <blackholll@163.com>
2020-05-24 17:48:30 +0800

    sphinx doc for r1.0.6

--- settings/common.py
+++ settings/common.py
@@ -14,7 +14,7 @@ import os
 import platform
 
 
-VERSION = 'v1.0.5'
+VERSION = 'v1.0.6'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
