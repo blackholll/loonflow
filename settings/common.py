c6cf711aa4792cb9a2e4f953939e884fd9f796c4
blackholll <blackholll@163.com>
2020-06-16 07:08:11 +0800

    update version for v1.0.9

--- settings/common.py
+++ settings/common.py
@@ -14,7 +14,7 @@ import os
 import platform
 
 
-VERSION = 'v1.0.8'
+VERSION = 'v1.0.9'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
