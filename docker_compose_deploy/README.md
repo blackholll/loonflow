4987cf9439b1d4a0ee2ac4b303f8b8775e0841aa
blackholll <blackholll@163.com>
2021-03-10 22:48:54 +0800

    docker compose for r1.1.0

--- docker_compose_deploy/README.md
+++ docker_compose_deploy/README.md
@@ -1,52 +1,44 @@
-本目录下提供多种docker compose方式部署方式
+
+为了方便的数据的持久化以及升级操作,本部署方案需要使用外部数据库(mysql)，请事先准备好数据库
+
 
 # 准备工作
-- 准备一台linux服务器
+- 准备一台linux系统的服务器
 - 安装好python3(请自行百度或者google)
 - 安装好docker-compose(请自行百度或者google)
 - 配置容器镜像加速(请自行百度或者google)
+- 准备好数据库，并授予权限
 
-# loonflow_only
-此启动方式将只会部署loonflow(包括nginx、redis)。为了方便的数据的持久化以及升级操作,请事先准备好数据库
-(注意创建库的使用使用utf-8字符集)。只部署loonflow(包括nginx、redis),启动时将连接到你提供的数据库
-(保证持久化数据),此方式可以直接用于生产环境
-
-### 准备好数据库，授予权限
 ```
 # 进入mysql后创建数据库并授权
-mysql> create database loonflow character set utf8;  # 注意要使用utf8字符集
+mysql> create database loonflow character set utf8mb4;  # 注意要使用utf8mb4字符集
 mysql> grant all privileges on loonflow.* to loonflow@'%' identified by '123456';
 
 ```
+- 修改run.py中相关配置
+```
+db_host = ''  # loonflow使用的数据库的ip
+db_port = ''  # loonflow使用的数据库的端口
+db_name = ''  # loonflow使用的数据库的名称
+db_user = ''  # loonflow使用的数据库的用户
+db_password = ''  # loonflow使用的数据库的用户密码
 
-#### 启动方式
-确保已经安装了python3后。 cd 到 docker_compose_deploy/loonflow_only目录后,执行以下命令
+ddl_db_user = ''  # 可以执行ddl(拥有修改表结构权限)的用户
+ddl_db_password = ''  # 可以执行ddl(拥有修改表结构权限)的用户的密码
 
 ```
-# 修改run.py中数据库相关配置为你准备好的数据库信息
-    db_host = ''  # loonflow使用的数据库的ip
-    db_port = ''  # loonflow使用的数据库的端口
-    db_name = ''  # loonflow使用的数据库的名称
-    db_user = ''  # loonflow使用的数据库的用户
-    db_password = ''  # loonflow使用的数据库的用户密码
-    
-    ddl_db_user = ''  # 可以执行ddl(拥有修改表结构权限)的用户
-    ddl_db_password = ''  # 可以执行ddl(拥有修改表结构权限)的用户的密码
 
+
+#### 启动方式
+cd 到 docker_compose_deploy目录后,执行以下命令
+
+```
 # 安装并启动服务
 python3 run.py install # 此命令后修改dockerfile中的数据库配置，然后启动
 
 # 启动服务
 python3 run.py start  # 此命令直接启动服务，请保证之前install过（也就是dockerfile中数据库配置已被修改）
 
-# 停止服务， 这种方式对于celery task任务非优雅停止，可以使用flower(celery的监控系统)，将任务消费停止，并且等待所有认为都结束后再执行
+# 停止服务， 这种方式对于celery task任务非优雅停止，可以使用flower(celery的监控系统)，将任务消费停止，并且等待所有任务都结束后再执行
 python3 run.py stop
 ```
-
-# loonflow_shutongflow
-
-暂未支持
-
-# loonflow_workflowdemo
-
-暂未支持
--- docker_compose_deploy/run.py
+++ docker_compose_deploy/run.py
@@ -27,7 +27,7 @@ def stop_compose():
     停止compose服务
     :return:
     """
-    home_path = os.path.abspath(os.path.join(os.getcwd(), "..")) + '/loonflow_only'
+    home_path = os.path.abspath(os.path.join(os.getcwd(), "..")) + '/loonflow'
     cmd_str = 'cd {}&&docker-compose stop'.format(home_path)
     flag, result = run_cmd(cmd_str)
     if flag:
@@ -45,10 +45,11 @@ def update_db_config(db_host, db_port, db_name, db_user, db_password):
     """
     print('-' * 30)
     print('开始更新数据库配置')
-    home_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    # home_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    home_path = os.getcwd()
 
     for file_dir in ('loonflow-task', 'loonflow-web'):
-        dockerfile_path = home_path + '/loonflow_only/{}/Dockerfile'.format(file_dir)
+        dockerfile_path = home_path + '/{}/Dockerfile'.format(file_dir)
         with open(dockerfile_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
         with open(dockerfile_path, "w", encoding="utf-8") as f_w:
@@ -83,7 +84,7 @@ def init_db(db_host, db_name, ddl_db_user, ddl_db_password):
     """
     print('-'*30)
     print('开始导入初始化sql，需要下载镜像arey/mysql-client, 请耐心等候')
-    init_sql = os.path.abspath(os.path.join(os.getcwd(), "../..")) + '/loonflow_init.sql'
+    init_sql = os.path.abspath(os.path.join(os.getcwd(), "..")) + '/loonflow1.1.0_init.sql'
     cmd_str = 'docker run -i arey/mysql-client -h{} -p{} -u{} {} < {}'.format(db_host, ddl_db_password, ddl_db_user,db_name, init_sql)
     print(cmd_str)
     run_cmd(cmd_str)
@@ -96,7 +97,9 @@ def start():
     开始启动
     :return:
     """
-    home_path = os.path.abspath(os.path.join(os.getcwd(), "..")) + '/loonflow_only'
+    # home_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+    home_path = os.getcwd()
+    print('homepath:', home_path)
     cmd_str = 'cd {}&&docker-compose up -d'.format(home_path)
     flag, result = run_cmd(cmd_str)
     if flag:
@@ -104,18 +107,18 @@ def start():
         print('启动成功，你可以直接通过http://hostip来访问loonflow的管理后台,其中hostip为你宿主机的ip')
     else:
         print('x'*30)
-        print('启动失败,请确认已经安装了docker compose后检查报错信息')
+        print('启动失败:{}'.format(result))
 
 
 if __name__ == '__main__':
-    db_host = ''  # loonflow使用的数据库的ip
-    db_port = ''  # loonflow使用的数据库的端口
-    db_name = ''  # loonflow使用的数据库的名称
-    db_user = ''  # loonflow使用的数据库的用户
-    db_password = ''  # loonflow使用的数据库的用户密码
+    db_host = '192.168.50.136'  # loonflow使用的数据库的ip
+    db_port = '3306'  # loonflow使用的数据库的端口
+    db_name = 'loonflow_1_1test'  # loonflow使用的数据库的名称
+    db_user = 'loonflow_1_1test'  # loonflow使用的数据库的用户
+    db_password = '123456'  # loonflow使用的数据库的用户密码
 
-    ddl_db_user = ''  # 可以执行ddl(拥有修改表结构权限)的用户
-    ddl_db_password = ''  # 可以执行ddl(拥有修改表结构权限)的用户的密码
+    ddl_db_user = 'loonflow_1_1test'  # 可以执行ddl(拥有修改表结构权限)的用户
+    ddl_db_password = '123456'  # 可以执行ddl(拥有修改表结构权限)的用户的密码
 
     if not (db_host and db_name and db_password and db_user):
         raise Exception('请配置的你的数据库地址、端口、数据库名称、用户、密码')
@@ -137,3 +140,7 @@ if __name__ == '__main__':
         stop_compose()
     else:
         raise Exception('usage: python3 run.py install/start/stop')
+
+
+
+
