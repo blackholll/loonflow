d34490c44092e2979d12acd2dda21d27de657d1d
blackholll <blackholll@163.com>
2021-03-13 10:48:04 +0800

    version in readme and config file

--- settings/common.py
+++ settings/common.py
@@ -14,7 +14,7 @@ import os
 import platform
 
 
-VERSION = 'v1.0.13'
+VERSION = '2.0.0'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
