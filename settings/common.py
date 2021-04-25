9c9bd3af5a984f3585177342827a227d289b8464
王飞 <wangfei@youzan.com>
2021-04-25 22:57:01 +0800

    release for 1.0.14

--- settings/common.py
+++ settings/common.py
@@ -14,7 +14,7 @@ import os
 import platform
 
 
-VERSION = 'v1.0.13'
+VERSION = 'v1.0.14'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
