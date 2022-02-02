3b6d92ed8682f576a4574be88cfc4b47126fbe03
blackholll <blackholll@163.com>
2022-02-02 15:53:08 +0800

    start v2.0.12

--- settings/common.py
+++ settings/common.py
@@ -14,7 +14,7 @@ import os
 import platform
 
 
-VERSION = '2.0.11'
+VERSION = '2.0.12'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
