59b89aaa5cb6762aee349ccb7d1d271a4ed396c7
blackholll <blackholll@163.com>
2022-10-18 22:55:00 +0800

    optimize: support redis server use no password

--- service/redis_pool.py
+++ service/redis_pool.py
@@ -6,6 +6,8 @@ redis_host = settings.REDIS_HOST
 redis_db = settings.REDIS_DB
 redis_port = settings.REDIS_PORT
 redis_password = settings.REDIS_PASSWORD
-
-POOL = redis.ConnectionPool(host=redis_host, port=redis_port, password=redis_password, max_connections=1000)
+if redis_password:
+    POOL = redis.ConnectionPool(host=redis_host, port=redis_port, password=redis_password, max_connections=1000)
+else:
+    POOL = redis.ConnectionPool(host=redis_host, port=redis_port, max_connections=1000)
 
